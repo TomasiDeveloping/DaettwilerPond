6fa75311aacd2c66108785ef2e6d98cf54167ad7
Tomasi - Developing <info@tomasi-developing.ch>
2023-05-18 16:52:37 +0200

    Add Spinner

--- UI/src/app/app.module.ts
+++ UI/src/app/app.module.ts
@@ -6,12 +6,15 @@ import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { HomeComponent } from './pages/home/home.component';
 import { CurrentTemperatureComponent } from './pages/temperature/current-temperature/current-temperature.component';
-import {HttpClientModule} from "@angular/common/http";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import { TemperatureComponent } from './pages/temperature/temperature.component';
 import { HistoryComponent } from './pages/history/history.component';
 import {HighchartsChartModule} from "highcharts-angular";
 import {DatePipe} from "@angular/common";
 import { NgxScrollTopModule } from 'ngx-scrolltop';
+import {NgxSpinnerModule} from "ngx-spinner";
+import {SpinnerInterceptor} from "./interceptors/spinner.interceptor";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
@@ -24,12 +27,17 @@ import { NgxScrollTopModule } from 'ngx-scrolltop';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     HttpClientModule,
     HighchartsChartModule,
-    NgxScrollTopModule
+    NgxScrollTopModule,
+    NgxSpinnerModule
   ],
-  providers: [DatePipe],
+  providers: [
+    DatePipe,
+    {provide: HTTP_INTERCEPTORS, useClass: SpinnerInterceptor, multi: true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
