97da90e1d5ad3c1d263a2e8f95dfff9022abb54c
Tomasi - Developing <info@tomasi-developing.ch>
2023-05-20 14:01:55 +0200

    DTO's for Authentication

---
+++ Application/DataTransferObjects/Authentication/RegistrationDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.DataTransferObjects.Authentication;
+
+public class RegistrationDto
+{
+    [Required(ErrorMessage = "Vorname ist ein Pflichtfeld")]
+    [MaxLength(200, ErrorMessage = "Vorname darf maximum 200 Zeichen lang sein")]
+    public string FirstName { get; set; }
+
+    [Required(ErrorMessage = "Nachname ist ein Pflichtfeld")]
+    [MaxLength(200, ErrorMessage = "Nachname darf maximum 200 Zeichen lang sein")]
+    public string LastName { get; set; }
+
+    [Required(ErrorMessage = "Email ist ein Pflichtfeld")]
+    [EmailAddress(ErrorMessage = "Keine gültige Email Adresse")]
+    public string Email { get; set; }
+
+    public string Password { get; set; }
+}
