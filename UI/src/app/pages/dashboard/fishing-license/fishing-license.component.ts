bf3ee277b81523a0b5773027011339f6f0956e93
Tomasi - Developing <info@tomasi-developing.ch>
2023-06-10 16:07:59 +0200

    Fix Show Remaining Days of License

--- UI/src/app/pages/dashboard/fishing-license/fishing-license.component.ts
+++ UI/src/app/pages/dashboard/fishing-license/fishing-license.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnChanges, SimpleChanges} from '@angular/core';
 import {FishingLicense} from "../../../models/fishingLicense.model";
 import * as moment from "moment";
 
@@ -7,12 +7,12 @@ import * as moment from "moment";
   templateUrl: './fishing-license.component.html',
   styleUrls: ['./fishing-license.component.scss']
 })
-export class FishingLicenseComponent implements OnInit {
+export class FishingLicenseComponent implements OnChanges {
 
   @Input() public fishingLicence: FishingLicense | undefined;
   public expiresInDays: number | undefined;
 
-  ngOnInit() {
+  ngOnChanges(changes: SimpleChanges) {
     if (this.fishingLicence) {
       if (new Date(this.fishingLicence.expiresOn) > new Date()) {
         this.expiresInDays = moment(this.fishingLicence?.expiresOn).diff(Date.now(), 'days');
